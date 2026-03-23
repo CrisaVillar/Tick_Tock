c3da7802ea50201da99f80915c31bcb75c1acda7
CrisaVillar <crmo.villar.ui@phinmaed.com>
2026-03-23 22:25:08 +0800

    Change database user and password in conn.js
    Updated database connection credentials for security.

--- conn.js
+++ conn.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'CrisaVillar',                
+  user: 'tick_tock1_user',                
   host: 'tick-tock-db.onrender.com',
   database: 'tick_tock1',      
-  password: '',                  
+  password: 'nOIbhJL0aHPohahg43swCU12L6JT2nbo',                  
   port: 5432                    
 });
 
