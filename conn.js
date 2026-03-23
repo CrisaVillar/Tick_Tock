58759f66e98dd09a50d1c9cd4b6d49580e9967c2
CrisaVillar <crmo.villar.ui@phinmaed.com>
2026-03-23 21:20:41 +0800

    Refactor database connection to use environment variables
    Updated database connection configuration to use environment variables for host, user, password, database, and port.

--- conn.js
+++ conn.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "tick_tock1"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-conn.connect((err)=> {
-    if(err) throw err;
-    console.log('Database Connected!');
+conn.connect((err) => {
+  if(err) throw err;
+  console.log("Database Connected!");
 });
 
 module.exports = conn;
