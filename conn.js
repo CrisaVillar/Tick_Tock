92927882bd121c132abc0dfcd18bfebed2e3011b
CrisaVillar <crmo.villar.ui@phinmaed.com>
2026-03-24 08:20:59 +0800

    Update conn.js

--- conn.js
+++ conn.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'tick_tock1_user',                
-  host: 'tick-tock-db.onrender.com',
+  host: 'dpg-d70jv0vgi27c73fjvutg-a.oregon-postgres.render.com',
   database: 'tick_tock1',      
   password: 'nOIbhJL0aHPohahg43swCU12L6JT2nbo',                  
   port: 5432                    
